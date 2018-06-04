5002908a39da02072030379536c0f335a4587b9b
刘一凡 <36919957+fangfucdwin@users.noreply.github.com>
2018-06-04 18:52:20 +0800

    Update ins_pic_craw_test.py

--- ins_pic_craw_test.py
+++ ins_pic_craw_test.py
@@ -16,7 +16,7 @@ kv = {
             'referer': 'https://www.instagram.com/',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'zh-CN,zh;q=0.9',
-            'cookie':'ds_user_id=7835676456; csrftoken=z5yp7F7g7AApLBGWQUsNDiu34c48j5NF; shbid=8778; rur=ATN; sessionid=IGSCf1391681e97905f7a957d125894583f3c191115565cd1adb4355760592be42dd%3AII76wH33uF4BeIOeXm3ZYs60V6lXUa5z%3A%7B%22_auth_user_id%22%3A7835676456%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%227835676456%3ALYhvsUR47Z7M61ksdm0YSKUcyoBAuiO9%3Ad4440c309c046f0980db215d1066ed3002932e8e4dc98a0e3d191ba865308e1c%22%2C%22last_refreshed%22%3A1527504417.3028142452%7D; mid=WwveFwALAAHWbyG__sTFw6noH6n3; mcd=3; fbm_124024574287414=base_domain=.instagram.com; urlgen="{\"time\": 1527504407\054 \"65.49.225.176\": 25820}:1fNFnV:LtI0lwhjY-dLK3wMQfD_1sxb140"; fbsr_124024574287414=aciYeBVvspIqaeLxn2CRSx0mza0ayhjLXcWqvk6uUPU.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImNvZGUiOiJBUURScE1tY3lPdExQMkxXb1lORDNoeTF0c3J3ZzRBSnNSZ0VqUS00a21veXhCTjlPRUd1eXd0MUItMXdiR28yX3kwS1RYU0RBMVNvbzZpcVZPOUNGQTdnbWx6Rk1Md3JMMmctOURxa3pfVHd2XzN1eGkzM0NjR3p1TDQtVnZVSmZSMW12ZkUxUFg5ZWxNT0tRdl9yMEl2Vll5UjlvTFJMRUlwaUQ4S3BxUVFmNUVkMXJwVWRkRS1laDQ4aXhXMXFtN0s0M2g1eXpaR0U3WUVmbTJCVDlxam9uMFVDOVQyZko0MjEtVXJJWGs0WXZRUzlqbzhwLVNHejdsWEdJMnEyeW5LT2NGVVppUS1BbTJoVEdvSjU0MmZvMVIzR3pucC1GVDZCZ3JMd2RfeDZqX084am1MRE4yUEw5NlhyWHhYT2NPZmNJSTRfYVg0RWRHVkVCLTZUcTZxeCIsImlzc3VlZF9hdCI6MTUyNzUwNTAzOCwidXNlcl9pZCI6IjEwMDAyNTY3NzM1MDc5NSJ9'
+            'cookie':'获取登陆cookie'
 }
 pics_list = []
 
@@ -111,14 +111,12 @@ fout.write("<html>")
 fout.write("<body>")
 fout.write("<ul>")
 for data in pics_list:
-    # 屏蔽掉原来的，重新写一个更美观的输出效果
     fout.write("<li>")
     #fout.write("<td>%s</td>" % data['URL'])
     #fout.write("<td>%s</td>" % data['Like_Count'])
     #fout.write("<td>%s</td>" % data['Commen_Count'])
     fout.write('<a href="%s" target="_blank">%s</a>' % (data['URL'],data['Num']))
     fout.write('<img src="%s">' % data['URL'])
-    #fout.write('<p>%s</p>' % data['summary'])
     fout.write("</li>")
 fout.write("</ul>")
 fout.write("</body>")
